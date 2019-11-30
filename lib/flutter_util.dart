0308fea05f189f9c8c054657dc2dffa2a3a9075e
lyy1005664097 <1005664097@qq.com>
2019-11-30 17:03:18 +0800

    flutter package 的 example

--- lib/flutter_util.dart
+++ lib/flutter_util.dart
@@ -10,7 +10,7 @@ export 'src/permission/misc.dart';
 
 export 'src/widgets/my_future_builder.dart';
 export 'src/widgets/my_outline_button.dart';
-export 'src/widgets/my_separator.dart';
+export 'src/widgets/separator.dart';
 export 'src/widgets/my_tab_bar.dart';
 export 'src/widgets/exception.dart';
 export 'src/widgets/waiting.dart';
